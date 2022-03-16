35cf6d6774907036729d4c32711134cdb78dd533
Guilherme Garibotti <guigaribotti@MacBook-Pro-de-Guilherme.local>
2022-03-16 00:51:50 -0300

    Primeiro commit

---
+++ imc.js
@@ -0,0 +1,32 @@
+
+function calcular() {
+    let nome = document.getElementById("nome").value;
+    let altura = document.getElementById("altura").value; // parseFloat
+    let peso = document.getElementById("peso").value; // parseFloat
+
+    if ((nome !== '') && (altura !== '') && (peso !== '')) {
+        let imc = (peso / altura ** 2).toFixed(2);
+
+        if (imc < 18.5) {
+            document.getElementById("resultado").innerHTML = nome + " a classificação do seu peso é " + "'Magreza'." + " Seu IMC é: " + imc;
+        } else if (imc < 25) {
+            document.getElementById("resultado").innerHTML = nome + " a classificação do seu peso é " + "'Normal'." + " Seu IMC é: " + imc;
+        } else if (imc < 30) {
+            document.getElementById("resultado").innerHTML = nome + " a classificação do seu peso é " + "'Sobrepeso'." + " Seu IMC é: " + imc;
+        } else if (imc < 35) {
+            document.getElementById("resultado").innerHTML = nome + " a classificação do seu peso é " + "'Obesidade'." + " Seu IMC é: " + imc;
+        } else if (imc < 40) {
+            document.getElementById("resultado").innerHTML = nome + " a classificação do seu peso é " + "'Obesidade Grau II'." + " Seu IMC é: " + imc;
+        }
+
+        //else {
+        //  document.getElementById("resultado").innerHTML = nome + " a classificação do seu peso é " + "'Obesidade Grau III'." + " Seu IMC é: " + imc;
+        //}
+
+    } else {
+        alert("Por favor preencha todos os campos.");
+
+    }
+
+
+}
